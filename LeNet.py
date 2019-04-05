c7c90aaa794fde49c85377961536883d7ac770f4
Yi Liang <lygg725@163.com>
2019-04-05 21:15:19 +0100

    Create LeNet.py

---
+++ LeNet.py
@@ -0,0 +1,41 @@
+import keras
+from keras.datasets import mnist
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten
+from keras.models import Sequential
+
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+
+KTF.set_session(tf.Session(config=tf.ConfigProto(device_count={'cpu':0})))
+# if your want to use gpu
+# KTF.set_session(tf.Session(config=tf.ConfigProto(device_count={'gpu':1})))
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+x_train = x_train / 255.
+x_test = x_test / 255.
+
+x_train = x_train.reshape(-1, 28, 28, 1)
+x_test=x_test.reshape(-1,28,28,1)
+
+lenet = Sequential()
+lenet.add(Conv2D(6, kernel_size=5, strides=1, padding='valid', input_shape=(28, 28, 1)))
+lenet.add(MaxPool2D(pool_size=2, strides=2))
+lenet.add(Conv2D(16, kernel_size=5, strides=1, padding='valid'))
+lenet.add(MaxPool2D(pool_size=2, strides=2))
+lenet.add(Flatten())
+lenet.add(Dense(120))
+lenet.add(Dense(84))
+lenet.add(Dense(10, activation='softmax'))
+
+
+lenet.summary()
+
+
+lenet.compile('sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+lenet.fit(x_train, y_train, batch_size=64, epochs=1, validation_data=[x_test, y_test])
+weights = lenet.get_weights()
+lenet.save('myletnet.h5')
