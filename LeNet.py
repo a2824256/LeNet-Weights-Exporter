43dc43dad64d9494c98005803aae6ea8489fb77a
Yi Liang <lygg725@163.com>
2019-04-18 05:48:23 +0100

    Update LeNet.py

--- LeNet.py
+++ LeNet.py
@@ -1,6 +1,6 @@
 import keras
 from keras.datasets import mnist
-from keras.layers import Conv2D, MaxPool2D, Dense, Flatten
+from keras.layers import Conv2D, AvgPool2D, Dense, Flatten
 from keras.models import Sequential
 
 import tensorflow as tf
@@ -22,9 +22,9 @@ x_test=x_test.reshape(-1,28,28,1)
 
 lenet = Sequential()
 lenet.add(Conv2D(6, kernel_size=5, strides=1, padding='valid', input_shape=(28, 28, 1)))
-lenet.add(MaxPool2D(pool_size=2, strides=2))
+lenet.add(AvgPool2D(pool_size=2, strides=2))
 lenet.add(Conv2D(16, kernel_size=5, strides=1, padding='valid'))
-lenet.add(MaxPool2D(pool_size=2, strides=2))
+lenet.add(AvgPool2D(pool_size=2, strides=2))
 lenet.add(Flatten())
 lenet.add(Dense(120))
 lenet.add(Dense(84))
